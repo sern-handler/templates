f830d0fd38d9ed91f40ad37bcf48f53c8b571787
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2022-12-08 17:00:16 -0600

    feat!: prepare templates for 2.0

--- templates/javascript-esm/src/index.js
+++ templates/javascript-esm/src/index.js
@@ -1,5 +1,5 @@
 import { Client, GatewayIntentBits } from 'discord.js';
-import { Sern } from '@sern/handler';
+import { Sern, single, DefaultLogging } from '@sern/handler';
 
 const client = new Client({
 	intents: [
@@ -9,12 +9,30 @@ const client = new Client({
 		GatewayIntentBits.MessageContent, // Make sure this is enabled for text commands!
 	],
 });
+
+/**
+ * Where all of your dependencies are composed. 
+ * '@sern/client' is usually your Discord Client.
+ * View documentation for pluggable dependencies
+ * Configure your dependency root to your liking.
+ * It follows the npm package iti https://itijs.org/.
+ * Use this function to access all of your dependencies.
+ * This is used for external event modules as well 
+ */
+export const useContainer = Sern.makeDependencies({	
+    build: root => root
+        .add({ '@sern/client': single(client)  })
+        .add({ '@sern/logger': single(new DefaultLogging()) })
+});
+
 //View docs for all options
 Sern.init({
-	client,
 	defaultPrefix: '!', // removing defaultPrefix will shut down text commands
 	commands: 'src/commands',
 	// events: 'src/events' (optional),
+	containerConfig : {
+		get: useContainer
+	}
 });
 
 client.login();
--- templates/typescript/src/index.ts
+++ templates/typescript/src/index.ts
@@ -1,5 +1,5 @@
 import { Client, GatewayIntentBits } from 'discord.js';
-import { Sern } from '@sern/handler';
+import { Dependencies, Sern, single, Singleton, DefaultLogging } from '@sern/handler'
 
 const client = new Client({
 	intents: [
@@ -9,12 +9,35 @@ const client = new Client({
 		GatewayIntentBits.MessageContent, //Make sure this is enabled for text commands!
 	],
 });
+
+//With typescript, you can customize / augment your typings.
+interface MyDependencies extends Dependencies {
+    '@sern/client' : Singleton<Client>;
+    '@sern/logger' : Singleton<DefaultLogging>
+}
+/**
+ * Where all of your dependencies are composed. 
+ * '@sern/client' is usually your Discord Client.
+ * View documentation for pluggable dependencies
+ * Configure your dependency root to your liking.
+ * It follows the npm package iti https://itijs.org/.
+ * Use this function to access all of your dependencies.
+ * This is used for external event modules as well 
+ */
+export const useContainer = Sern.makeDependencies<MyDependencies>({
+    build: root => root
+       .add({ '@sern/client' : single(client)  })
+       .add({ '@sern/logger': single(new DefaultLogging()) })
+});
+
 //View docs for all options
 Sern.init({
-	client,
 	defaultPrefix: '!', // removing defaultPrefix will shut down text commands
-	commands: 'dist/commands',
-	// events: 'dist/events' (optional),
+	commands: 'src/commands',
+	// events: 'src/events' (optional),
+	containerConfig : {
+		get: useContainer
+	}
 });
 
 client.login();
