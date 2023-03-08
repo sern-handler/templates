1c1e68387013be37595f4605d134cbf80717e580
Peter-MJ-Parker <34216187+Peter-MJ-Parker@users.noreply.github.com>
2023-03-08 03:18:42 -0600

    feat: bypasses mongoose connection if no uri
    Edit ready event so it will bypass the mongoose connection if there is no uri present

--- templates/ts-class-template/src/events/discord/client/ready.ts
+++ templates/ts-class-template/src/events/discord/client/ready.ts
@@ -1,13 +1,14 @@
-import { logger } from '#utils';
-import { EventType, eventModule } from '@sern/handler';
-import { Client, Events } from 'discord.js';
-import { mongoConnect } from '#handler';
+import { env, logger } from "#utils";
+import { EventType, eventModule } from "@sern/handler";
+import { Client, Events } from "discord.js";
+import { mongoConnect } from "#handler";
 
 export default eventModule({
 	type: EventType.Discord,
 	name: Events.ClientReady,
 	async execute(client: Client) {
 		logger().success(`[CLIENT] Logged into Discord as ${client.user?.tag}`);
+		if (!env.CONNECT) return;
 		mongoConnect();
 	},
 });
